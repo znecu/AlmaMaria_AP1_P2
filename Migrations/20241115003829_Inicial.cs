eae0c3ad59601bb974fd851a310cd85580576f07
znecu <alma_maria@ucne.edu.do>
2024-11-14 20:39:04 -0400

    Migracion inicial lista

---
+++ Migrations/20241115003829_Inicial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlmaMaria_AP1_P2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Registro",
+                columns: table => new
+                {
+                    RegistroId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registro", x => x.RegistroId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registro");
+        }
+    }
+}
