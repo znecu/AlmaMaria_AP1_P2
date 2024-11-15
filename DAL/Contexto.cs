f6e6254cad1b90d68c6ff8e2ba0c04fc4d7a6ba6
znecu <alma_maria@ucne.edu.do>
2024-11-14 20:35:31 -0400

    Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,6 +1,11 @@
-﻿namespace AlmaMaria_AP1_P2.DAL
+﻿using AlmaMaria_AP1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlmaMaria_AP1_P2.DAL;
+
+public class Contexto :DbContext
 {
-    public class Contexto
-    {
-    }
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Registro> Registro { get; set; }   
 }
