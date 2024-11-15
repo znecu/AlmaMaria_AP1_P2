3a817857b00cbf1885fa94a1fdb3a50f9f04529c
znecu <alma_maria@ucne.edu.do>
2024-11-14 20:43:19 -0400

    Plantilla del servicio

--- Services/RegistroServices.cs
+++ Services/RegistroServices.cs
@@ -1,6 +1,39 @@
-﻿namespace AlmaMaria_AP1_P2.Services
+﻿using AlmaMaria_AP1_P2.DAL;
+using AlmaMaria_AP1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlmaMaria_AP1_P2.Services
 {
-    public class RegistroServices
+    public class RegistroServices(IDbContextFactory<Contexto> DbFactory)
     {
+        public async Task<bool> Guardar(Registro registro)
+        {
+
+        }
+
+        private async Task<bool> Modificar(Registro registro)
+        {
+
+        }
+
+        private async Task<bool> Insertar(Registro preregistrostamo)
+        {
+
+        }
+
+        private async Task<bool> Existe(int registroId)
+        {
+
+        }
+
+        public async Task<bool> Eliminar(int registroId)
+        {
+
+        }
+
+        public async Task<Registro?> Buscar(int registroId)
+        {
+
+        }
     }
 }
