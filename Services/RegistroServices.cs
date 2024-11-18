b620cb96143f0c2b5c7672b949f69a540e671450
Alma Judith Maria Marte <alma_maria@ucne.edu.do>
2024-11-18 18:06:12 -0400

    Comenté la plantilla del services

--- Services/RegistroServices.cs
+++ Services/RegistroServices.cs
@@ -4,36 +4,36 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AlmaMaria_AP1_P2.Services
 {
-    public class RegistroServices(IDbContextFactory<Contexto> DbFactory)
-    {
-        public async Task<bool> Guardar(Registro registro)
-        {
+    //public class RegistroServices(IDbContextFactory<Contexto> DbFactory)
+    //{
+    //    public async Task<bool> Guardar(Registro registro)
+    //    {
 
-        }
+    //    }
 
-        private async Task<bool> Modificar(Registro registro)
-        {
+    //    private async Task<bool> Modificar(Registro registro)
+    //    {
 
-        }
+    //    }
 
-        private async Task<bool> Insertar(Registro preregistrostamo)
-        {
+    //    private async Task<bool> Insertar(Registro preregistrostamo)
+    //    {
 
-        }
+    //    }
 
-        private async Task<bool> Existe(int registroId)
-        {
+    //    private async Task<bool> Existe(int registroId)
+    //    {
 
-        }
+    //    }
 
-        public async Task<bool> Eliminar(int registroId)
-        {
+    //    public async Task<bool> Eliminar(int registroId)
+    //    {
 
-        }
+    //    }
 
-        public async Task<Registro?> Buscar(int registroId)
-        {
+    //    public async Task<Registro?> Buscar(int registroId)
+    //    {
 
-        }
-    }
+    //    }
+    //}
 }
